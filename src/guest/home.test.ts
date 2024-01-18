14ae3f31df7d1fb0bc82b90d29d08aa7bb2f50ec
ahmadfawaz1505 <halafaz810@gmail.com>
2024-01-18 17:43:26 +0700

    update

--- src/guest/home.test.ts
+++ src/guest/home.test.ts
@@ -1,2 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
+
+test("hasil scan inspect", async () => {
+  const result = await db.inspection_record.findMany({
+    where: {},
+    include: {
+      location: {
+        select: {
+          name: true,
+        },
+        include: {
+          m_asset: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  // console.log(result);
+});
