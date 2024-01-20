596934ed1807be02ddd9e6f75f3dcda37b1f5f9a
LutfiGiantoro <Mohammad.Lutfi.Giantoro@hashmicro.com>
2024-01-21 00:13:42 +0700

    home test

--- src/admin/home.test.ts
+++ src/admin/home.test.ts
@@ -3,52 +3,11 @@ const db = new PrismaClient();
 
 test("list inspection schedule", async () => {
   const inp_schedule = await db.inspection_schedule.findMany({
-    where: {},
     select: {
       id: true,
       name: true,
       scheduled_at: true,
-    },
-  });
-
-  console.log(inp_schedule);
-});
-
-test("list maintenance schedule", async () => {
-  const result = await db.maintenance_schedule.findMany({
-    where: {},
-    select: {
-      id: true,
-      name: true,
-      scheduled_at: true,
-    },
-  });
-
-  //   console.log(result);
-});
-
-// test("hasil scan inspect", async () => {
-//   const result = await db.inspection_record.findMany({
-//     include: {
-//       m_user: {
-//         select: {
-//           name: true,
-//         },
-//       },
-//     },
-//     select: {
-//       result: true,
-//     },
-//   });
-
-//   console.log(result);
-// });
-
-test("list follow up", async () => {
-  const result = await db.inspection_record.findMany({
-    select: {
-      id: true,
-      m_asset: {
+      m_asset:{
         select: {
           name: true,
           category: {
@@ -56,29 +15,16 @@ test("list follow up", async () => {
               name: true,
             },
           },
-          location: {
-            select: {
-              name: true,
-            },
-          },
         },
       },
-      result: true,
+      m_user: {
+        select: {
+          name: true,
+        },
+      },
     },
   });
 
-    console.log(result);
+  console.log(inp_schedule);
 });
 
-test("Maintenance Tracking", async () => {
-  const result = await db.maintenance_schedule.findMany({
-    where: {},
-    select: {
-      id: true,
-      name: true,
-      scheduled_at: true,
-    },
-  });
-
-  //   console.log(result);
-});
