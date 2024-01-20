df982adcd84ac8fcced3ab8e76382ecf970eeeca
LutfiGiantoro <Mohammad.Lutfi.Giantoro@hashmicro.com>
2024-01-20 17:33:37 +0700

    update

--- src/admin/maintenance_schedule.test.ts
+++ src/admin/maintenance_schedule.test.ts
@@ -1,17 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
+
+//audit_trail -> id, ts, by, activity, table, meta
 test("create maintenance schedule", async () => {
   const mtn_schedule = await db.maintenance_schedule.create({
     data: {
         name: "Jadwal maintenance rutin",
         scheduled_at: "2024-09-14 00:00:00",
-        created_at: new Date(),
         created_by: "67f64a2c-8ca6-49fe-94b2-de07ca907768",
-        updated_at: "",
-        updated_by: "",
-        m_user: {
+        assigned_to: {
             connect: {id: ""}
+            // connectOrCreate: {create: {}, where: {id : ""}}
         },
         m_asset: {
             connect: {id: ""}
