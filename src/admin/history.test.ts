bbdcecbd5f21b613c1534d9be9f1c0b19427bde7
LutfiGiantoro <Mohammad.Lutfi.Giantoro@hashmicro.com>
2024-01-24 10:51:02 +0700

    update

--- src/admin/history.test.ts
+++ src/admin/history.test.ts
@@ -1,95 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
-test("History", async () => {
-    const history = await db.inspection_record.findMany({
-      where: {m_asset: {id: "fa95ef66-9024-4268-9a2e-f764ff9000c9"}},
-      select: {
-        m_asset: {
-          select: {
-            id: true,
-            name: true,
-            brand: true,
-            type: true,
-            serial_number: true,
-            dimension: true,
-            uom: true,
-            manufacture_year: true,
-            description: true,
-            latitude: true,
-            longitude: true,
-            condition_status: true,
-            category: {
-                select: {
-                    id: true,
-                    name: true,
-                },
-              },
-          },
-        },
-        location: {
-          select: {
-            id: true,
-            name: true,
-          },
-        },
-        //reported issue
-        inspection_detail: {
-            select: {
-                id: true,
-                m_parameter_inspection: {
-                    select: {
-                        id: true,
-                        name: true,
-                    },
-                },
-            },
-        },
-        result: true,
-        m_user: {
-          select: {
-            id: true,
-            name: true,
-          },
-        },
-        // maintenance ticket
-        maintenance_order: {
-            select: {
-                id: true,
-                created_at: true,
-                name: true,
-                mo_number: true,
-                m_user:{
-                    select:{
-                        id: true,
-                        name: true,
-                    },
-                },
-        //maintenance plan
-                mo_plan: {
-                    select: {
-                        id: true,
-                        name: true,
-                        price: true,
-                        m_maintenance_task: {
-                            select: {
-                                id: true,
-                                name: true,
-                            },
-                        },
-                        //mop_material: {},
-                    },
-                },
-            },
-        },
-        // maintenance record
-      },
-    });
-    //  console.log(history);
-  });
-
-
-test("History", async () => {
+test("Histori inspection", async () => {
   const history_inspection = await db.m_asset.findMany({
     where: {
       id: "fa95ef66-9024-4268-9a2e-f764ff9000c9",
@@ -167,4 +79,26 @@ test("History maintenance ticket", async () => {
   console.log(history_mtn_ticket);
 });
 
+
+test("History maintenance plan", async () => {
+  const history_mtn_plan = await db.mo_plan.findFirst({
+    where: {
+      inspection_record: {
+        id: "e7d47cbf-9112-449d-946a-d69aad0884bb",
+      },
+    },
+    select: {
+      id: true,
+      mo_number: true,
+      name: true,
+      m_user: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
+  console.log(history_mtn_plan);
+});
   
